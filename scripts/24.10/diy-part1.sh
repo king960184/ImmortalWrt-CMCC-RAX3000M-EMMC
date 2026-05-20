5bede9ddf9fd76587f54dc0a73f1f9ca9a9aa9de
king960184 <70425136+king960184@users.noreply.github.com>
2026-05-20 21:09:53 +0800

    更新 diy-part1.sh

--- scripts/24.10/diy-part1.sh
+++ scripts/24.10/diy-part1.sh
@@ -19,6 +19,16 @@ rm -rf feeds/packages/lang/rust
 # 添加 OpenClash 官方源
 git clone --depth=1 -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
+# iStore商店
+git clone https://github.com/linkease/istore.git package/istore
+git clone https://github.com/linkease/luci-app-store.git package/luci-app-store
+
+# 快速启动向导
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+cp -rf package/openwrt-packages/luci-app-quickstart package/
+cp -rf package/openwrt-packages/luci-i18n-quickstart-zh-cn package/
+rm -rf package/openwrt-packages
+
 # 修正权限
 # 确保脚本在编译前有正确的执行权限
 chmod -R 755 package/luci-app-openclash
